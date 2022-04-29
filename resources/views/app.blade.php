660eb7c1f7fa4c470a59a0d1a28b0766c2524505
chrislkeefer <42353349+chrislkeefer@users.noreply.github.com>
2022-04-29 11:55:31 -0700

    🧬 scaffolding for front-end work

---
+++ resources/views/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://rsms.me/inter/inter.css" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body id="app" class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+                <div class="flex-shrink">
+                    
+                </div>
+                <div class="w-full flex-grow justify-center flex items-center">
+                    <img src="{{ asset('images/brand-logo.png') }}" />
+                </div>
+                <div class="flex-shrink">
+                    
+                </div>
+            </div>
+        </header>
+
+        <!-- Page Content -->
+        <main>
+            
+        </main>
+    </div>
+</body>
+
+</html>
